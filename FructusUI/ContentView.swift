482d3d6b749fb7404c63f0d014445f00d1b8b4b2
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-11-06 13:49:25 -0300

    Initial Commit

---
+++ FructusUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FructusUI
+//
+//  Created by Daniel Washington Ignacio on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FructusUI/FructusUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusUIApp.swift
+//  FructusUI
+//
+//  Created by Daniel Washington Ignacio on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
